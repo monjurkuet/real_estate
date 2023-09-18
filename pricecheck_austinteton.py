b61e1b9afe603313350cd8f6def995ccbf28a6c8
monjurkuet <34279740+monjurkuet@users.noreply.github.com>
2023-09-19 03:16:40 +0600

    Update

--- pricecheck_austinteton.py
+++ pricecheck_austinteton.py
@@ -6,45 +6,48 @@ from seleniumwire.utils import decode
 import time,getpass,platform,random
 from dateutil import parser
 from datetime import timedelta
+import sqlite3
+import re
 
-INCOGNITO=0
-PROXY=0
-
+# Constants for file paths
+USER_DATA_DIR_WINDOWS = "D:\\airbnbscrapingprofile"
+USER_DATA_DIR_LINUX = f"/home/{getpass.getuser()}/airbnbscrapingprofile"
+BROWSER_EXECUTABLE_PATH_WINDOWS = 'C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe'
+BROWSER_EXECUTABLE_PATH_LINUX = '/usr/bin/brave-browser'
+# Constants for API URLs
+DETAILS_API = 'https://www.airbnb.com/api/v3/StaysPdpSections'
+# Connect to the database (creates a new one if it doesn't exist)
+conn = sqlite3.connect('database.db')
+cursor = conn.cursor()
+# waitfor
 def waitfor(driver,xpth):
     try: 
         WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, xpth)))
     except:
         pass 
-
-def newBrowser():
-    SYSTEM_OS=platform.system()
-    if SYSTEM_OS=='Windows':
-        user_data_dir="D:\\airbnbscrapingprofile"
-        browser_executable_path='C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe'
-    if SYSTEM_OS=='Linux':
-        CURRENTUSER=getpass.getuser()
-        user_data_dir=f"/home/{CURRENTUSER}/airbnbscrapingprofile"
-        browser_executable_path='/usr/bin/brave-browser'
-    options = uc.ChromeOptions()
-    if INCOGNITO==1:
-        options.add_argument("--incognito")
-    if PROXY==1:
-        driver=uc.Chrome(user_data_dir=user_data_dir,
-                        browser_executable_path=browser_executable_path,options=options,
-                        headless=False,seleniumwire_options={
-            'proxy': {
-                'http': "http://45.85.147.136:24003",
-                'https': "http://45.85.147.136:24003"
-                    }
-                })
-    else:
-        driver=uc.Chrome(user_data_dir=user_data_dir,
-                        browser_executable_path=browser_executable_path,options=options,
-                        headless=False)
+#extract data
+def extract_realtime_data(driver):
+    details_content = None
+    for request in driver.requests:
+        if request.response:
+            if DETAILS_API in request.url and request.response.status_code == 200:
+                details_content = eval(decode(request.response.body, request.response.headers.get('Content-Encoding', 'identity')).decode('utf-8'))
+    if details_content is not None:
+        details_content = details_content['data']
+    return details_content
+# Create a reusable browser function
+def new_browser():
+    SYSTEM_OS = platform.system()
+    if SYSTEM_OS == 'Windows':
+        user_data_dir = USER_DATA_DIR_WINDOWS
+        browser_executable_path = BROWSER_EXECUTABLE_PATH_WINDOWS
+    elif SYSTEM_OS == 'Linux':
+        user_data_dir = USER_DATA_DIR_LINUX
+        browser_executable_path = BROWSER_EXECUTABLE_PATH_LINUX
+    driver = uc.Chrome(user_data_dir=user_data_dir, browser_executable_path=browser_executable_path, headless=False)
     return driver
 
-def get_listings():
-    cursor = connection.cursor()         
+def get_listings():       
     cursor.execute("SELECT * FROM `availability` WHERE available=1")
     rows=cursor.fetchall()   
     row_headers=[x[0] for x in cursor.description] 
@@ -54,27 +57,25 @@ def get_listings():
     print('Total rows : ',len(rows))
     return initial_data
 
-def update_listing_price(listingId,calendarDate,price,pricewithfees):
-    cursor = connection.cursor()    
+def update_listing_price(listingId,calendarDate,price,pricewithfees):  
     sql_select_query = """Update availability set price= %s,pricewithfees= %s where listingId = %s and calendarDate = %s """
     val=(price,pricewithfees,listingId,calendarDate)
     cursor.execute(sql_select_query,val)
-    connection.commit()
+    conn.commit()
     print(val)
 
 def update_datedata(listingId,calendarDate,priceDate,Price):
-    cursor = connection.cursor()    
     sql_insert_with_param = """REPLACE INTO datedata
                             (listingId,calendarDate,priceDate,Price) 
                             VALUES (%s,%s,%s,%s);"""
     val = (listingId,calendarDate,priceDate,Price)
     cursor.execute(sql_insert_with_param , val)
-    connection.commit() 
+    conn.commit() 
     print(val)
 
 if __name__ == "__main__":
     listings=get_listings()
-    driver=newBrowser()
+    driver=new_browser()
     COUNTER=0
     for listing in listings:
         COUNTER+=1
@@ -82,7 +83,7 @@ if __name__ == "__main__":
             try:
                 driver.close()
                 driver.quit()
-                driver=newBrowser()
+                driver=new_browser()
             except Exception as e:
                 print(e) 
         try:
@@ -93,9 +94,14 @@ if __name__ == "__main__":
             checkoutdate=checkindate+ timedelta(days=minNights)
             listingurl=f'https://www.airbnb.com/rooms/{listingId}?check_in={checkindate.strftime("%Y-%m-%d")}&check_out={checkoutdate.strftime("%Y-%m-%d")}'
             driver.get(listingurl)
-            waitfor('//div[@data-section-id="BOOK_IT_SIDEBAR"]//span[@class="_tyxjp1"]')
+            waitfor(driver,'//div[@data-section-id="BOOK_IT_SIDEBAR"]//span[@class="_tyxjp1"]')
             time.sleep(random.uniform(1,10))
-            price=driver.find_element('xpath','//div[@data-section-id="BOOK_IT_SIDEBAR"]//span[@class="_tyxjp1"]').text.strip().split('$')[1].replace(',','')
+            details_content=extract_realtime_data(driver)
+            try:
+                details_content.presentation.stayProductDetailPage.sections.sections[0].section.structuredDisplayPrice
+                price=details_content['presentation']['stayProductDetailPage']['sections']['sections'][0]['section']['structuredDisplayPrice']['primaryLine']['discountedPrice']
+            except:
+                price=re.search(r'"discountedPrice":(.*?),"', str(details_content)).group(1).strip().split('$')[1].replace(',','')
             pricewithfees=0
             update_listing_price(listingId,calendarDate,price,pricewithfees)
             for i in range(minNights):
