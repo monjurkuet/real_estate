8f47fb8e7ba4053661f7d2138e52c9b59d513754
monjurkuet <34279740+monjurkuet@users.noreply.github.com>
2023-09-19 00:22:05 +0600

    Update create table

--- createtable.py
+++ createtable.py
@@ -2,39 +2,49 @@ import sqlite3
 
 # Connect to the database (creates a new one if it doesn't exist)
 conn = sqlite3.connect('database.db')
-
-# Create a cursor object to execute SQL commands
 cursor = conn.cursor()
-
-# Define the table name
-table_name1 = 'listings'
-
-# Define the column names and their data types
-columns1 = [
-    'listingId TEXT',
-    'listingLat FLOAT',
-    'listingLng FLOAT',
-    'bedType TEXT',
-    'roomType TEXT',
-    'personCapacity INTEGER',
-    'descriptionLanguage TEXT',
-    'isSuperhost INTEGER',
-    'accuracyRating FLOAT',
-    'checkinRating FLOAT',
-    'cleanlinessRating FLOAT',
-    'communicationRating FLOAT',
-    'locationRating FLOAT',
-    'valueRating FLOAT',
-    'guestSatisfactionOverall FLOAT',
-    'visibleReviewCount INTEGER',
-    'location TEXT',
-    'title TEXT'
-]
-
-# Create the table if it doesn't exist
-create_table1_query = f"CREATE TABLE IF NOT EXISTS {table_name1} ({', '.join(columns1)})"
-cursor.execute(create_table1_query)
-
-# Commit the changes and close the connection
+# Define the CREATE listings TABLE statement
+create_table_sql = """
+CREATE TABLE IF NOT EXISTS "listings" (
+    "listingId" TEXT,
+    "listingLat" FLOAT,
+    "listingLng" FLOAT,
+    "bedType" TEXT,
+    "roomType" TEXT,
+    "personCapacity" INTEGER,
+    "descriptionLanguage" TEXT,
+    "isSuperhost" INTEGER,
+    "accuracyRating" FLOAT,
+    "checkinRating" FLOAT,
+    "cleanlinessRating" FLOAT,
+    "communicationRating" FLOAT,
+    "locationRating" FLOAT,
+    "valueRating" FLOAT,
+    "guestSatisfactionOverall" FLOAT,
+    "visibleReviewCount" INTEGER,
+    "title" TEXT,
+    "timestamp" DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY("listingId")
+);
+"""
+# Execute the CREATE TABLE statement
+cursor.execute(create_table_sql)
 conn.commit()
+# Define the CREATE availability TABLE statement 
+create_table_sql = """
+CREATE TABLE IF NOT EXISTS "availability" (
+    "listingId" TEXT,
+    "available" INTEGER,
+    "calendarDate" DATETIME,
+    "maxNights" INTEGER,
+    "minNights" INTEGER,
+    "availableForCheckin" INTEGER,
+    "bookable" INTEGER,
+    "timestamp" DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+"""
+# Execute the CREATE TABLE statement
+cursor.execute(create_table_sql)
+conn.commit()
+#close connection
 conn.close()
