459ba18d42e1e4162c1b75caeb096f6cee3054a4
monjurkuet <34279740+monjurkuet@users.noreply.github.com>
2023-09-25 04:04:06 +0600

    Airdna search crawler

--- airdna/search.py
+++ airdna/search.py
@@ -1,18 +1,20 @@
 import seleniumwire.undetected_chromedriver as uc
 from seleniumwire.utils import decode
+from selenium.webdriver.common.keys import Keys
 import time
 import getpass
 import platform
 import sqlite3
 from config import AIRDNA_EMAIL,AIRDNA_PASSWORD
+
 # Constants for API URLs
 DETAILS_API = 'https://api.airdna.co/api/explorer/v1/market/'
 LOGIN_URL='https://app.airdna.co/data/login'
+
 # Constants for file paths
 BROWSER_EXECUTABLE_PATH_WINDOWS = "C:\\Users\\muham\\AppData\\Local\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"
 #BROWSER_EXECUTABLE_PATH_WINDOWS = 'C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe'
 BROWSER_EXECUTABLE_PATH_LINUX = '/usr/bin/brave-browser'
-
 
 # Connect to the database (creates a new one if it doesn't exist)
 conn = sqlite3.connect('database.db')
@@ -21,57 +23,26 @@ cursor = conn.cursor()
 # Create a reusable browser function
 def new_browser():
     SYSTEM_OS = platform.system()
-    if SYSTEM_OS == 'Windows':
-        browser_executable_path = BROWSER_EXECUTABLE_PATH_WINDOWS
-    elif SYSTEM_OS == 'Linux':
-        browser_executable_path = BROWSER_EXECUTABLE_PATH_LINUX
+    browser_executable_path = (
+        BROWSER_EXECUTABLE_PATH_WINDOWS if SYSTEM_OS == 'Windows' else BROWSER_EXECUTABLE_PATH_LINUX
+    )
     driver = uc.Chrome(browser_executable_path=browser_executable_path, headless=False)
     return driver
+
 # Create a reusable function for extracting real-time data
 def extract_realtime_data(driver):
-    availability_content = None
-    details_content = None
+    details_content=None
+    #Fix variables for python
+    null=None
+    true=True
+    false=False
     for request in driver.requests:
         if request.response:
-            if DETAILS_API in request.url and request.response.status_code == 200:
-                details_content = eval(decode(request.response.body, request.response.headers.get('Content-Encoding', 'identity')).decode('utf-8'))
-    if availability_content is not None and details_content is not None:
-        availability_content = availability_content['data']
-        details_content = details_content['data']
-    return availability_content, details_content
-
-def insert_details(listingId,listingLat,listingLng,bedType,roomType,personCapacity,descriptionLanguage,
-                    isSuperhost,accuracyRating,checkinRating,cleanlinessRating,communicationRating,locationRating,
-                    valueRating,guestSatisfactionOverall,visibleReviewCount,title):
-    # Insert the data into the "listings" table
-    cursor.execute('''REPLACE INTO listings (
-        listingId, listingLat, listingLng, bedType, roomType, personCapacity, descriptionLanguage,
-        isSuperhost, accuracyRating, checkinRating, cleanlinessRating, communicationRating, locationRating,
-        valueRating, guestSatisfactionOverall, visibleReviewCount, title
-    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
-    (
-        listingId, listingLat, listingLng, bedType, roomType, personCapacity, descriptionLanguage,
-        isSuperhost, accuracyRating, checkinRating, cleanlinessRating, communicationRating, locationRating,
-        valueRating, guestSatisfactionOverall, visibleReviewCount, title
-    ))
-    conn.commit() 
-    print(f"Crawled PropertyId : {listingId}")
-
-def insert_availability(listingId,available,calendarDate,maxNights,minNights,availableForCheckin,bookable):
-    sql_insert_with_param = """REPLACE INTO availability
-                            (listingId,available,calendarDate,maxNights,minNights,availableForCheckin,bookable) 
-                            VALUES (?, ?, ?, ?, ?, ?, ?);"""
-    val = (listingId,available,calendarDate,maxNights,minNights,availableForCheckin,bookable)
-    cursor.execute(sql_insert_with_param , val)
-    conn.commit() 
-    print(val)
-
-#ITEMS_LIST=[i.get_attribute('href') for i in driver.find_elements('xpath','//div[@id="FMP-target"]//div[@data-testid="card-container"]/a')]
-ITEMS_LIST=["https://www.airbnb.com/rooms/plus/19017960?display_currency=USD",
-            "https://www.airbnb.com/rooms/689828220112367064?display_currency=USD"]
-
-driver=new_browser()
-login(driver)
+            if DETAILS_API in request.url and request.response.status_code == 200 and 'listings' in request.url :
+                details_content = eval(decode(request.response.body, request.response.headers.get('Content-Encoding', 'identity')).decode('utf-8'))['payload']['listings']
+                break
+    del driver.requests
+    return details_content
 
 def login(driver):
     driver.get(LOGIN_URL)
@@ -80,50 +51,54 @@ def login(driver):
     driver.find_element('xpath','//input[@name="password"]').send_keys(AIRDNA_PASSWORD)
     driver.find_element('xpath','//input[@name="password"]').send_keys(Keys.ENTER)
     time.sleep(10)
+    
+def insert_property(data):
+    insert_statement = """
+        REPLACE INTO airdna_search (
+            property_id, airbnb_property_id, vrbo_property_id, listing_type, bedrooms, bathrooms,
+            accommodates, rating, reviews, title, revenue_ltm, revenue_potential_ltm,
+            occupancy_rate_ltm, average_daily_rate_ltm, days_available_ltm, market_id,
+            market_name, latitude,longitude, currency
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    """
+    # Execute the INSERT statement with data from the dictionary
+    cursor.execute(insert_statement, (
+    data["property_id"], data["airbnb_property_id"], data["vrbo_property_id"],
+    data["listing_type"], data["bedrooms"], data["bathrooms"], data["accommodates"],
+    data["rating"], data["reviews"], data["title"], data["revenue_ltm"],
+    data["revenue_potential_ltm"], data["occupancy_rate_ltm"],
+    data["average_daily_rate_ltm"], data["days_available_ltm"],
+    data["market_id"], data["market_name"],
+    data["location"]['lat'],data["location"]['lng'],  data["currency"]
+    ))
+    print(data["airbnb_property_id"])
+
+driver=new_browser()
+login(driver)
 
 SEARCH_URL='https://app.airdna.co/data/us/79503?tab=active-str-listings'
 driver.get(SEARCH_URL)
+RETRYCOUNTER=0
+while True and RETRYCOUNTER!=5:
+    details_content=extract_realtime_data(driver)
+    if details_content is None:
+        RETRYCOUNTER+=1
+    else:
+        for data in details_content:
+            try:
+                insert_property(data)
+            except:
+                pass
+        conn.commit() 
+    print('Scrolling....')
+    # Scroll down to bottom
+    driver.execute_script("arguments[0].scrollIntoView();", driver.find_elements(By.XPATH, '//img[@alt="carousel_image_0"]')[-1])
+    # Wait to load page
+    time.sleep(10)
+    RETRYCOUNTER-=1
 
-for each_listing in ITEMS_LIST:
-    print(each_listing)
-    driver.get(each_listing)
-    time.sleep(20)
-    availability_content,details_content=extract_realtime_data(driver)
-    if availability_content and details_content:
-        # property data
-        title=driver.find_element('xpath','//h1').text.strip()
-        metadata=details_content['presentation']['stayProductDetailPage']['sections']['metadata']['loggingContext']['eventDataLogging']
-        listingId=metadata['listingId']
-        listingLat=metadata['listingLat']
-        listingLng=metadata['listingLng']
-        bedType=metadata['bedType']
-        roomType=metadata['roomType']
-        personCapacity=metadata['personCapacity']
-        descriptionLanguage=metadata['descriptionLanguage']
-        isSuperhost=metadata['isSuperhost']
-        accuracyRating=metadata['accuracyRating']
-        checkinRating=metadata['checkinRating']
-        cleanlinessRating=metadata['cleanlinessRating']
-        communicationRating=metadata['communicationRating']
-        locationRating=metadata['locationRating']
-        valueRating=metadata['valueRating']
-        guestSatisfactionOverall=metadata['guestSatisfactionOverall']
-        visibleReviewCount=metadata['visibleReviewCount']
-        insert_details(listingId,listingLat,listingLng,bedType,roomType,personCapacity,descriptionLanguage,
-        isSuperhost,accuracyRating,checkinRating,cleanlinessRating,communicationRating,locationRating,
-        valueRating,guestSatisfactionOverall,visibleReviewCount,title)
-        # availability data
-        calendarMonths=availability_content['merlin']['pdpAvailabilityCalendar']['calendarMonths']
-        for eachmonth in calendarMonths:
-            for day in eachmonth['days']:
-                calendarDate=day['calendarDate']
-                available=day['available']
-                maxNights=day['maxNights']
-                minNights=day['minNights']
-                availableForCheckin=day['availableForCheckin']
-                bookable=day['bookable']
-                insert_availability(listingId,available,calendarDate,maxNights,minNights,availableForCheckin,bookable)
-    del driver.requests
+    
+
 
 conn.close()
 driver.close()
--- createtable.sql
+++ createtable.sql
@@ -9,7 +9,7 @@ CREATE TABLE "availability" (
 	"bookable"	INTEGER,
 	"timestamp"	DATETIME DEFAULT CURRENT_TIMESTAMP,
 	UNIQUE("listingId","calendarDate")
-)
+);
 --
 CREATE TABLE "datedata" (
 	"listingId"	TEXT,
@@ -18,7 +18,7 @@ CREATE TABLE "datedata" (
 	"Price"	REAL,
 	"booked_days"	TEXT,
 	UNIQUE("listingId","calendarDate","priceDate","booked_days")
-)
+);
 --
 CREATE TABLE "listings" (
     "listingId" TEXT,
@@ -40,6 +40,31 @@ CREATE TABLE "listings" (
     "title" TEXT,
     "timestamp" DATETIME DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY("listingId")
-)
+);
 --
-CREATE VIEW "average_daily_price" AS SELECT listingId,calendarDate,price,avg(price) FROM datedata GROUP by listingId,calendarDate
+CREATE VIEW "average_daily_price" AS SELECT listingId,calendarDate,price,avg(price) FROM datedata GROUP by listingId,calendarDate;
+--
+CREATE TABLE [airdna_search] ( 
+  [property_id] VARCHAR(255) NOT NULL,
+  [airbnb_property_id] VARCHAR(255) NOT NULL,
+  [vrbo_property_id] VARCHAR(255) NULL,
+  [listing_type] VARCHAR(255) NULL,
+  [bedrooms] INT NULL,
+  [bathrooms] DECIMAL(3, 1) NULL,
+  [accommodates] INT NULL,
+  [rating] DECIMAL(3, 1) NULL,
+  [reviews] INT NULL,
+  [title] VARCHAR(255) NULL,
+  [revenue_ltm] DECIMAL(10, 2) NULL,
+  [revenue_potential_ltm] DECIMAL(10, 2) NULL,
+  [occupancy_rate_ltm] DECIMAL(5, 2) NULL,
+  [average_daily_rate_ltm] DECIMAL(10, 2) NULL,
+  [days_available_ltm] INT NULL,
+  [market_id] INT NULL,
+  [market_name] VARCHAR(255) NULL,
+  [currency] VARCHAR(10) NULL,
+  [latitude] FLOAT NULL,
+  [longitude] FLOAT NULL,
+  [updated_at] DATETIME NULL DEFAULT CURRENT_TIMESTAMP ,
+   PRIMARY KEY ([airbnb_property_id])
+);
