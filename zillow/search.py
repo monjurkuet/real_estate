6d091743d8271c3a69c5c190223340271187ef73
monjurkuet <34279740+monjurkuet@users.noreply.github.com>
2023-10-04 21:07:12 +0600

    Update element parsing

--- zillow/search.py
+++ zillow/search.py
@@ -76,7 +76,7 @@ for each_property in queue[:1]:
     time.sleep(random.uniform(3,9))
 
 with open('zillowdata.html','r') as f:
-        htmldata=f.read()
+    htmldata=f.read()
 
 # Assuming you have the HTML content as a string
 html_content = htmldata  # Replace this with your HTML content
@@ -102,48 +102,49 @@ rentZestimate = rent_zestimate_element.get_text(strip=True).replace('$', '').rep
 tax_assessed_value = None
 property_tax = None
 
-if 'Tax assessment' in html_content:
-    df = pd.read_html(io.StringIO(html_content))[0]
-    if 'Tax assessment' in df.columns:
-        tax_assessed_value = df['Tax assessment'][0].split(' ')[0].replace('$', '').replace(',', '')
-    if 'Property taxes' in df.columns:
-        property_tax = df['Property taxes'][0].split(' ')[0].replace('$', '').replace(',', '')
+
+tax_assessed_value = re.search(r'"taxAssessedValue\\":(\d+)', html_content)
+if tax_assessed_value:
+    tax_assessed_value = tax_assessed_value.group(1)
+property_tax = re.search(r'"taxPaid\\":(\d+\.\d+)', html_content)
+if property_tax:
+    property_tax = property_tax.group(1)
 
 # Extract livingArea and lotAreaValue
-living_area_match = re.search(r'Total interior livable area:\s*(\d{1,3}(?:,\d{3})*(?:\.\d+)?)\s*sqft', html_content)
+living_area_match = re.search(r'"livingArea\\":(\d+)', html_content)
 if living_area_match:
     livingArea = living_area_match.group(1).replace(',', '')
 else:
     livingArea = None
 
-lot_area_value_match = re.search(r'Lot size:\s*(\d{1,3}(?:,\d{3})*(?:\.\d+)?)\s*sqft', html_content)
+lot_area_value_match = re.search(r'"lotSize\\":(\d+)', html_content)
 if lot_area_value_match:
     lotAreaValue = lot_area_value_match.group(1).replace(',', '')
 else:
     lotAreaValue = None
 
 # Extract bathrooms and bedrooms
-bathrooms_element = soup.find(text='bd').find_parent()
-bathrooms = bathrooms_element.get_text(strip=True).split(' ')[0]
+bathrooms_element = soup.find(string='bd').find_parent().find_parent()
+bathrooms = bathrooms_element.get_text(strip=True).replace('bd', '')
 
-bedrooms_element = soup.find(text='ba').find_parent()
-bedrooms = bedrooms_element.get_text(strip=True).split(' ')[0]
+bedrooms_element = soup.find(string='ba').find_parent().find_parent()
+bedrooms = bedrooms_element.get_text(strip=True).replace('ba', '')
 
 # Extract latitude, longitude, and year_built
-latitude_match = re.search(r'"latitude":(-?\d+\.\d+)', html_content)
+latitude_match = re.search(r'"latitude\\":(-?\d+\.\d+)', html_content)
 if latitude_match:
     latitude = latitude_match.group(1)
 else:
     latitude = None
 
-longitude_match = re.search(r'"longitude":(-?\d+\.\d+)', html_content)
+longitude_match = re.search(r'"longitude\\":(-?\d+\.\d+)', html_content)
 if longitude_match:
     longitude = longitude_match.group(1)
 else:
     longitude = None
 
-year_built_element = soup.find(text='Year built:').find_next_sibling()
-year_built = year_built_element.get_text(strip=True)
+year_built_element = re.search(r'"yearBuilt\\":(\d+)', html_content)
+year_built = year_built_element.group(1)
 
 domain=extract_data(htmldata)
 savetodatabase(linkedin_url,domain)
