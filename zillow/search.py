440ef3d73f7d58442650228a808896401ff100cf
monjurkuet <34279740+monjurkuet@users.noreply.github.com>
2023-10-09 20:06:49 +0600

    Update

--- zillow/search.py
+++ zillow/search.py
@@ -1,14 +1,12 @@
 import pyautogui
-import time,platform,random
+import time,random
 import re
 from tqdm import tqdm
 import pandas as pd
 import io
 import subprocess
-import signal
 import os
 import sqlite3
-from bs4 import BeautifulSoup
 from selenium import webdriver   
 from selenium.webdriver.common.by import By
 
@@ -18,8 +16,6 @@ SEARCH_URL='https://www.zillow.com/homes/'
 # Connect to the database (creates a new one if it doesn't exist)
 conn = sqlite3.connect('database.db')
 cursor = conn.cursor()
-
-browserprocess = subprocess.Popen(browserprocess_command, stdout=subprocess.PIPE,shell=True, preexec_fn=os.setsid, stderr=subprocess.PIPE)
 
 def enter_navbar_text(textvalue):
     subprocess.run(['wmctrl','-xa','brave'])
@@ -44,66 +40,67 @@ def get_listings():
     print('Total rows : ',len(rows))
     return initial_data
 
+def extract_data(queue):
+    for each_property in tqdm(queue):
+        geoid=each_property['geoid']
+        address=each_property['display_name']
+        enter_navbar_text(SEARCH_URL+address)
+        time.sleep(15)
+        driver.get('file:///home/shovon/Desktop/vm shared folder/github/airbnb/zillow/zillowdata.html')
+        if 'No matching results' in driver.page_source:
+            continue
+        if not driver.find_elements(By.XPATH,'//button[text()="Zestimate"]/following::*'):
+            continue
+        zpid=re.search(r'"zpid":"(\d+)"', driver.page_source).group(1)
+        zestimate=driver.find_element(By.XPATH,'//button[text()="Zestimate"]/following::*').text.replace('$','').replace(',','')
+        if driver.find_elements(By.XPATH,'//button[text()="Rent Zestimate"]/following::*'):
+            rentZestimate=driver.find_element(By.XPATH,'//button[text()="Rent Zestimate"]/following::*').text.replace('$','').replace(',','')
+        else:
+            rentZestimate=None
+        buffer = io.StringIO(driver.page_source)
+        if 'Tax assessment' in driver.page_source:
+            taxAssessedValue=pd.read_html(buffer)[-1]['Tax assessment'][0].split(' ')[0].replace('$','').replace(',','')
+        else:
+            taxAssessedValue=None
+        if 'Property taxes' in driver.page_source:
+            property_tax=pd.read_html(buffer)[-1]['Property taxes'][0].split(' ')[0].replace('$','').replace(',','')
+        else:
+            property_tax=None
+        if 'Total interior livable area' in driver.page_source:
+            livingArea=re.search(r'Total interior livable area:\s*(\d{1,3}(?:,\d{3})*(?:\.\d+)?)\s*sqft', driver.page_source).group(1).replace(',','')
+        else:
+            livingArea=None
+        if re.search(r'Lot size:\s*(\d{1,3}(?:,\d{3})*(?:\.\d+)?)\s*sqft', driver.page_source):
+            lotAreaValue=re.search(r'Lot size:\s*(\d{1,3}(?:,\d{3})*(?:\.\d+)?)\s*sqft', driver.page_source).group(1).replace(',','')
+        else:
+            lotAreaValue=None
+        if driver.find_elements(By.XPATH,'//span[text()="bd"]//parent::*'):
+            bathrooms=driver.find_element(By.XPATH,'//span[text()="bd"]//parent::*').text.split(' ')[0]
+        else:
+            bathrooms=None
+        if driver.find_elements(By.XPATH,'//span[text()="ba"]//parent::*'):
+            bedrooms=driver.find_element(By.XPATH,'//span[text()="ba"]//parent::*').text.split(' ')[0]
+        else:
+            bedrooms=None
+        latitude=re.search(r'"latitude\\":(-?\d+\.\d+)', driver.page_source).group(1)
+        longitude=re.search(r'"longitude\\":(-?\d+\.\d+)', driver.page_source).group(1)
+        if driver.find_elements(By.XPATH,'//span[text()="Year built:"]/following::*'):
+            year_built=driver.find_element(By.XPATH,'//span[text()="Year built:"]/following::*').text
+        else:
+            year_built=None
+        # Define the INSERT statement
+        sqlite_insert_with_param = """REPLACE INTO zillow
+                            (zpid, geoid, zestimate, rentZestimate, taxAssessedValue, livingArea, lotAreaValue, bathrooms, bedrooms, latitude, longitude, year_built, property_tax) 
+                            VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?);""" 
+        data_tuple = (zpid, geoid, zestimate, rentZestimate, taxAssessedValue, livingArea, lotAreaValue, bathrooms, bedrooms, latitude, longitude, year_built, property_tax)
+        cursor.execute(sqlite_insert_with_param, data_tuple)
+        # Commit the changes to the database
+        conn.commit()
+        print(data_tuple)
+        time.sleep(random.uniform(3,9))
+        os.remove('zillowdata.html')
+    driver.quit()
+
 queue=get_listings()
 driver = webdriver.Chrome()
-
-for each_property in queue[63:]:
-    geoid=each_property['geoid']
-    address=each_property['display_name']
-    enter_navbar_text(SEARCH_URL+address)
-    time.sleep(15)
-    driver.get('file:///home/shovon/Desktop/shared folder/airbnb/zillow/zillowdata.html')
-    if 'No matching results' in driver.page_source:
-        continue
-    if not driver.find_elements(By.XPATH,'//button[text()="Zestimate"]/following::*'):
-        continue
-    zpid=re.search(r'"zpid":"(\d+)"', driver.page_source).group(1)
-    zestimate=driver.find_element(By.XPATH,'//button[text()="Zestimate"]/following::*').text.replace('$','').replace(',','')
-    if driver.find_elements(By.XPATH,'//button[text()="Rent Zestimate"]/following::*'):
-        rentZestimate=driver.find_element(By.XPATH,'//button[text()="Rent Zestimate"]/following::*').text.replace('$','').replace(',','')
-    else:
-        rentZestimate=None
-    buffer = io.StringIO(driver.page_source)
-    if 'Tax assessment' in driver.page_source:
-        taxAssessedValue=pd.read_html(buffer)[-1]['Tax assessment'][0].split(' ')[0].replace('$','').replace(',','')
-    else:
-        taxAssessedValue=None
-    if 'Property taxes' in driver.page_source:
-        property_tax=pd.read_html(buffer)[-1]['Property taxes'][0].split(' ')[0].replace('$','').replace(',','')
-    else:
-        property_tax=None
-    if 'Total interior livable area' in driver.page_source:
-        livingArea=re.search(r'Total interior livable area:\s*(\d{1,3}(?:,\d{3})*(?:\.\d+)?)\s*sqft', driver.page_source).group(1).replace(',','')
-    else:
-        livingArea=None
-    if re.search(r'Lot size:\s*(\d{1,3}(?:,\d{3})*(?:\.\d+)?)\s*sqft', driver.page_source):
-        lotAreaValue=re.search(r'Lot size:\s*(\d{1,3}(?:,\d{3})*(?:\.\d+)?)\s*sqft', driver.page_source).group(1).replace(',','')
-    else:
-        lotAreaValue=None
-    if driver.find_elements(By.XPATH,'//span[text()="bd"]//parent::*'):
-        bathrooms=driver.find_element(By.XPATH,'//span[text()="bd"]//parent::*').text.split(' ')[0]
-    else:
-        bathrooms=None
-    if driver.find_elements(By.XPATH,'//span[text()="ba"]//parent::*'):
-        bedrooms=driver.find_element(By.XPATH,'//span[text()="ba"]//parent::*').text.split(' ')[0]
-    else:
-        bedrooms=None
-    latitude=re.search(r'"latitude\\":(-?\d+\.\d+)', driver.page_source).group(1)
-    longitude=re.search(r'"longitude\\":(-?\d+\.\d+)', driver.page_source).group(1)
-    if driver.find_elements(By.XPATH,'//span[text()="Year built:"]/following::*'):
-        year_built=driver.find_element(By.XPATH,'//span[text()="Year built:"]/following::*').text
-    else:
-        year_built=None
-    # Define the INSERT statement
-    sqlite_insert_with_param = """REPLACE INTO zillow
-                        (zpid, geoid, zestimate, rentZestimate, taxAssessedValue, livingArea, lotAreaValue, bathrooms, bedrooms, latitude, longitude, year_built, property_tax) 
-                        VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?);""" 
-    data_tuple = (zpid, geoid, zestimate, rentZestimate, taxAssessedValue, livingArea, lotAreaValue, bathrooms, bedrooms, latitude, longitude, year_built, property_tax)
-    cursor.execute(sqlite_insert_with_param, data_tuple)
-    # Commit the changes to the database
-    conn.commit()
-    print(data_tuple)
-    time.sleep(random.uniform(3,9))
-    os.remove('zillowdata.html')
-
-driver.quit()
+extract_data(queue)
